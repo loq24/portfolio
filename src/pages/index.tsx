1bf4d1ab2c6c5b8a75f280eb29c5c4c137cd0fdf
Lougie <lougie.quisel@gmail.com>
2022-11-01 22:04:39 +0800

    Improve about me page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,48 +14,51 @@ const Home = () => {
       <div>
         <h1>About Me</h1>
         <p>
-          I&apos;m Lougie Quisel, a ReactJS, NextJS {`&`} React Native
-          developer. I&apos;m currently working at{" "}
+          Hi, I&apos;m Lougie Quisel and I love building websites. With years of
+          experience, I have built websites for a{" "}
+          <a
+            href="https://americanbb.bank/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            bank
+          </a>
+          , a website for a product that is being promoted by a{" "}
+          <a
+            href="https://whipshots.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            popular artist
+          </a>
+          ,{" "}
           <a
             href="https://thewoo.com/"
             rel="noopener noreferrer"
             target="_blank"
           >
-            TheWoo
-          </a>{" "}
-          as a Senior Web Developer. I also work at{" "}
+            to a company website
+          </a>
+          . I also love to challenge myself with my personal projects that you
+          can check on my{" "}
           <a
-            href="https://www.onlinejobs.ph/"
+            href="https://github.com/loq24"
             rel="noopener noreferrer"
             target="_blank"
           >
-            Onlinejobs.ph
-          </a>{" "}
-          maintaining the website with CodeIgniter framework.
+            github account
+          </a>
+          .
         </p>
         <p>
-          I have several small personal projects that you can check on my{" "}
-          <a
-            rel="noopener noreferrer"
-            href="https://github.com/loq24"
-            target="_blank"
-          >
-            Github
-          </a>{" "}
-          account. You can also check my React Native{" "}
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://play.google.com/store/apps/details?id=com.randomdumagueterestaurant"
-          >
-            app
-          </a>{" "}
-          too! If you want some help with your project, don't hesitate to{" "}
+          I have so many{" "}
           <Link href="/hire-me">
-            <a>contact me!</a>
-          </Link>
+            <a>skills</a>
+          </Link>{" "}
+          that I can offer if you want to work with me.{" "}
+          <a href="mailto:lougie.quisel@gmail.com">Contact me</a> if you are
+          interested.
         </p>
-        <p>When I&apos;m not working, I enjoy playing Badminton and DOTA 2!</p>
       </div>
     </>
   );
